1d43b58144d2b776c75ad919078690c59fe31211
qm853 <82948942+qm853@users.noreply.github.com>
2021-04-21 14:23:55 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    basic.showArrow(ArrowNames.North)
+    basic.pause(500)
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showString("Hei Tor!")
+})
+basic.forever(function () {
+	
+})
